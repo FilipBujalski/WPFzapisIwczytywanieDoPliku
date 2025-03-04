bed222822056ce46c6c64c05d61bce306e1485c4
Filip Bujalski <bujalski.filip@szkola.elektrykplock.edu.pl>
2025-03-04 12:57:37 +0100

    Dodałem projekt

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace danexml
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Pracownik> pracownicy = new List<Pracownik>()
+            {
+                new Pracownik() { Id = 1, Imie = "Jan", Nazwisko = "Kowalski", Zarobki = 12234, Data_zatrudnienia = new DateTime(2025, 3, 4), Data_zwolnienia = new DateTime(1971, 7, 23) },
+                new Pracownik() { Id = 2, Imie = "Piotr", Nazwisko = "Nowak", Zarobki = 9876, Data_zatrudnienia = new DateTime(2025, 3, 4), Data_zwolnienia = new DateTime(1971, 7, 23) },
+                new Pracownik() { Id = 3, Imie = "Ewa", Nazwisko = "Iksińska", Zarobki = 6543, Data_zatrudnienia = new DateTime(2025, 3, 4), Data_zwolnienia = new DateTime(1971, 7, 23) }
+            };
+
+            PracownicyLista.ItemsSource = pracownicy;
+        }
+
+        private void ZapiszDaneDoPliku(List<Pracownik> pracownicy)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Pracownik>));
+            using (StreamWriter writer = new StreamWriter("pracownicy.xml"))
+            {
+                serializer.Serialize(writer, pracownicy);
+            }
+        }
+
+        private void Zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            List<Pracownik> pracownicy = (List<Pracownik>)PracownicyLista.ItemsSource;
+            ZapiszDaneDoPliku(pracownicy);
+        }
+
+        private List<Pracownik> WczytajDaneZPliku()
+        {
+            if (File.Exists("pracownicy.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Pracownik>));
+                using (StreamReader reader = new StreamReader("pracownicy.xml"))
+                {
+                    return (List<Pracownik>)serializer.Deserialize(reader);
+                }
+            }
+            return null;
+        }
+
+        private void Wczytaj_Click(object sender, RoutedEventArgs e)
+        {
+            List<Pracownik> pracownicy = WczytajDaneZPliku();
+            if (pracownicy != null)
+            {
+                PracownicyLista.ItemsSource = pracownicy;
+            }
+            else
+            {
+                MessageBox.Show("Nie udało się wczytać danych z pliku.");
+            }
+        }
+    }
+}
---
+++ Pracownik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace danexml
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+
+        public string Imie { get; set; }
+
+        public string Nazwisko { get; set; }
+
+        public int Zarobki { get; set; }
+
+        public DateTime Data_zatrudnienia { get; set; }
+
+        public DateTime Data_zwolnienia { get; set; }
+    }
+}
